12b8dad61909f75e5cfe2e7dc91dd540c57bf156
davert <davert.githole@mailican.com>
2013-08-08 04:10:56 +0300

    disabling captcha in options

--- setup.sh
+++ setup.sh
@@ -14,7 +14,5 @@ ln -s .wp-cli/bin/wp wp
 ./wp term create "Game of Drones" category
 ./wp post create --post_type=page --post_status=publish --post_title='Submit a Post' --post_content="[user-submitted-posts]"
 
-# enabling "Game of Drones" category for user to submit post
-./wp option update usp_options '{"categories": [1,2]}' --format=json
-./wp option update usp_options '{"usp_captcha": "hide"}' --format=json
-./wp option update usp_options '{"usp_name": "show"}' --format=json
+# updating plugin options: enabling "Game of Drones" category, disabling captcha
+./wp option set usp_options '{"default_options":0,"author":1,"categories":["1"],"number-approved":-1,"redirect-url":"","error-message":"There was an error. Please ensure that you have added a title, some content, and that you have uploaded only images.","min-images":0,"max-images":1,"min-image-height":0,"min-image-width":0,"max-image-height":1500,"max-image-width":1500,"usp_name":"show","usp_url":"show","usp_title":"show","usp_tags":"show","usp_category":"show","usp_images":"hide","upload-message":"Please select your image(s) to upload.","usp_form_width":"300","usp_question":"1 + 1 =","usp_response":"2","usp_casing":0,"usp_captcha":"hide","usp_content":"show","success-message":"Success! Thank you for your submission.","usp_form_version":"current","usp_email_alerts":1,"usp_email_address":"davert.php@mailican.com","usp_use_author":0,"usp_use_url":0,"usp_use_cat":0,"usp_use_cat_id":"","usp_include_js":1,"usp_display_url":"","usp_form_content":"","usp_richtext_editor":0,"usp_featured_images":0}' --format=json
